548683831bf4082b1c30ab0d037bd78e6774d7f2
DeepshikhaKarn15 <121663201+DeepshikhaKarn15@users.noreply.github.com>
2022-12-30 15:50:26 +0530

    Add files via upload
    update

--- database/m2.sql
+++ database/m2.sql
@@ -32,7 +32,14 @@ CREATE TABLE IF NOT EXISTS `items` (
   CONSTRAINT `items_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
--- Data exporting was unselected.
+-- Dumping data for table api.items: ~3 rows (approximately)
+INSERT INTO `items` (`id`, `productname`, `description`, `userid`, `created_at`, `updated_at`) VALUES
+	(5, 'p_test_1', '206522', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+	(6, 'p_test_2', '206522', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+	(7, 'p_test_3', '206522', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+	(8, 'p_test_4', '206522', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+	(9, 'p_test_5', '206522', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+	(10, 'p_test_6', '206522', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- Dumping structure for table api.order_status
 CREATE TABLE IF NOT EXISTS `order_status` (
@@ -49,7 +56,14 @@ CREATE TABLE IF NOT EXISTS `order_status` (
   CONSTRAINT `order_status_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Data exporting was unselected.
+-- Dumping data for table api.order_status: ~1 rows (approximately)
+INSERT INTO `order_status` (`order_id`, `user_id`, `status`, `created_at`, `updated_at`, `id`) VALUES
+	(5, 1, 'pendding', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 2),
+	(6, 1, 'pendding', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 3),
+	(7, 2, 'pendding', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 4),
+	(8, 2, 'pendding', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 5),
+	(9, 3, 'pendding', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 6),
+	(10, 3, 'pendding', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 7);
 
 -- Dumping structure for table api.user
 CREATE TABLE IF NOT EXISTS `user` (
@@ -62,7 +76,11 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
--- Data exporting was unselected.
+-- Dumping data for table api.user: ~3 rows (approximately)
+INSERT INTO `user` (`id`, `username`, `password`, `created_at`, `updated_at`) VALUES
+	(1, 'testing_1', '1234', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+	(2, 'testing_2', '1234', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+	(3, 'testing_3', '12345', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
